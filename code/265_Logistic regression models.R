b9126539efe1fd1604544cff69d082975a8cac25
Paul G. Smith <pgsmith2000@gmail.com>
2019-11-10 15:31:41 -0600

    change figures

--- code/265_Logistic regression models.R
+++ code/265_Logistic regression models.R
@@ -66,5 +66,5 @@ p1 <- ggplot(Tidy_LogFinal,
   theme(legend.position = "none") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-ggsave("../output/figures/OddsProbFullLogModel.png", p1)
+ggsave("../output/figures/LogistModelOR.png", p1)
 
